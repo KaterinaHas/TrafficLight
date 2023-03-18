52f8a531387a553994ca91b8e53e32e8c847c177
ekaterinav <katyushka_vasina@mail.ru>
2023-03-18 20:13:28 +0300

    add logic for button trafficlight

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -8,18 +8,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var redLight: UIView!
     @IBOutlet weak var yellowLight: UIView!
     @IBOutlet weak var greenLight: UIView!
+    @IBOutlet weak var runTrafficLight: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        redLight.layer.cornerRadius = 75
+        yellowLight.layer.cornerRadius = 75
+        greenLight.layer.cornerRadius = 75
+        redLight.alpha = 1
     }
     
     @IBAction func RunTrafficLight(_ sender: Any) {
+        runTrafficLight.setTitle("Next", for: .normal)
+        if redLight.alpha == 1 {
+            redLight.alpha = 0.3
+            yellowLight.alpha = 1
+            greenLight.alpha = 0.3
+        } else if yellowLight.alpha == 1 {
+            redLight.alpha = 0.3
+            yellowLight.alpha = 0.3
+            greenLight.alpha = 1
+        } else if greenLight.alpha == 1 {
+            redLight.alpha = 1
+            yellowLight.alpha = 0.3
+            greenLight.alpha = 0.3
+        }
     }
-    
-
 }
-
